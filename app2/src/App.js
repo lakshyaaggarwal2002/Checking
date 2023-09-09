d54ace7a9aa979f88a24e5e53871882038e3d626
Lakshya Aggarwal <lakshyaaggarwalishere@gmail.com>
2023-09-10 01:48:18 +0530

    Add App2

---
+++ app2/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reloadsadfsafsafafasfasfnkjasdlgabnsfjlvbgkjlafvgjabgSunt consequat aute esse esse esse ea quis et reprehenderit aute excepteur irure. Velit est sit proident velit eiusmod anim. Ex culpa anim eu exercitation veniam exercitation fugiat nostrud ipsum. Laboris eu ullamco duis labore enim quis ad adipisicing tempor consectetur excepteur duis do. Proident deserunt occaecat officia labore Lorem in ea ullamco enim et qui sint laboris..
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
